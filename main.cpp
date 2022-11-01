5d3e6de03edb7b5a8ad9cc616fe1c81cd22892fd
Kimdongmin-sdt <dongmin.kim@sdt.inc>
2022-11-01 13:09:52 +0900

    update

--- main.cpp
+++ main.cpp
@@ -9,18 +9,20 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
+DigitalIn test_pin(PC_5);
+DigitalOut power_sw(PC_0);
+DigitalOut res_sw(PC_3);
 
 int main()
 {
+#if 0
     // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
-
     while (true) {
-        led = !led;
+        printf("val : %d\n", test_pin.read());
         ThisThread::sleep_for(BLINKING_RATE);
     }
+#endif
+    power_sw = 1;
+    res_sw = 1;
+
 }
